6826c556112aa68a611454ab6f8be2104df7f2f7
illyfrancis <illy.francis@outlook.com>
2014-03-26 14:42:09 -0400

    gulp

--- browserify2/simple/gulpfile.js
+++ browserify2/simple/gulpfile.js
@@ -10,12 +10,13 @@ gulp.task('default', function () {
 });
 
 gulp.task('test-main', function () {
-  var b = browserify('./src/main/js/app.js');
+  var b = browserify();
 
   glob.sync('./src/main/js/**/*.js').forEach(function (file) {
     b.require(file, { expose: '.' });
   });
 
+  b.add('./src/main/js/app.js');
   b.bundle()
     .pipe(source('g_core.js'))
     .pipe(gulp.dest('./target'));
@@ -24,14 +25,14 @@ gulp.task('test-main', function () {
 gulp.task('test-build', function () {
   var b = browserify();
 
-  // add all tests
-  glob.sync('./src/test/js/**/*.js').forEach(function (file) {
-    b.add(file);
-  });
-
   // externalize core
   glob.sync('./src/main/js/**/*.js').forEach(function (file) {
     b.external(file);
+  });
+
+  // add all tests
+  glob.sync('./src/test/js/**/*.js').forEach(function (file) {
+    b.add(file);
   });
 
   b.bundle()
